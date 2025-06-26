29a84c3e747409cbb6296c87e0180b1c1250ec1f
李碧拓 <2394520481@qq.com>
2025-06-26 17:51:03 +0800

    添加scss，路由，基础layout

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'
+
+
 
 export default defineConfig({
+  server: {host: '0.0.0.0',port: 6001,},
+  resolve: {alias: {'@': path.resolve(import.meta.dirname, 'src'),},},
   plugins: [react()],
   test: {
     globals: true,
